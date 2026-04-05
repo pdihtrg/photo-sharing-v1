5b9b2f8a4c21b5629ce4a1475b36fa484c57c3f9
Phạm Đình Trường <145275744+pdihtrg@users.noreply.github.com>
2026-04-05 02:29:41 +0000

    init

--- photo-sharing-v1/src/components/UserPhotos/index.jsx
+++ photo-sharing-v1/src/components/UserPhotos/index.jsx
@@ -1,23 +1,59 @@
 import React from "react";
+import { useParams, Link } from "react-router-dom";
+import models from "../../modelData/models";
 import { Typography } from "@mui/material";
 
-import "./styles.css";
-import {useParams} from "react-router-dom";
+function UserPhotos() {
 
-/**
- * Define UserPhotos, a React component of Project 4.
- */
-function UserPhotos () {
-    const user = useParams();
-    return (
-      <Typography variant="body1">
-        This should be the UserPhotos view of the PhotoShare app. Since it is
-        invoked from React Router the params from the route will be in property
-        match. So this should show details of user:
-        {user.userId}. You can fetch the model for the user
-        from models.photoOfUserModel(userId):
-      </Typography>
-    );
+  const { userId } = useParams();
+
+  const photos = models.photoOfUserModel(userId);
+
+  return (
+    <div>
+
+      {photos.map(photo => (
+
+        <div key={photo._id} style={{ marginBottom: 40 }}>
+
+          <img
+            src={`/images/${photo.file_name}`}
+            alt=""
+            style={{ width: "400px" }}
+          />
+
+          <Typography>
+            {new Date(photo.date_time).toLocaleString()}
+          </Typography>
+
+          {photo.comments && photo.comments.map(comment => (
+
+            <div key={comment._id} style={{ marginLeft: 20 }}>
+
+              <Typography variant="body2">
+                <Link to={`/users/${comment.user._id}`}>
+                  {comment.user.first_name} {comment.user.last_name}
+                </Link>
+              </Typography>
+
+              <Typography variant="caption">
+                {new Date(comment.date_time).toLocaleString()}
+              </Typography>
+
+              <Typography>
+                {comment.comment}
+              </Typography>
+
+            </div>
+
+          ))}
+
+        </div>
+
+      ))}
+
+    </div>
+  );
 }
 
 export default UserPhotos;
